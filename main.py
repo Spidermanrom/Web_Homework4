137d0c3cefdb8b1a814b756524d02f1aea4c8d86
Spidermanrom <93603589+Spidermanrom@users.noreply.github.com>
2024-05-22 17:40:45 +0300

    finally)

--- main.py
+++ main.py
@@ -1,86 +1,78 @@
-import mimetypes
-from pathlib import Path
-import urllib.parse
-import json
+from datetime import datetime
+from threading import Thread
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import threading
+import pathlib
 import socket
-import datetime
+import urllib.parse
+import mimetypes
+import json
 
 
-BASE_DIR = Path()
-
-HTTP_PORT = 3000
-HTTP_HOST = '0.0.0.0'
-UDP_PORT = 5000
 UDP_IP = '127.0.0.1'
-buffer_size = 1024
+UDP_PORT = 5000
+HTTP_PORT = 3000
+bufferSize = 1024
 
-class GoitFramework(BaseHTTPRequestHandler):
 
+class HttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        route = urllib.parse.urlparse(self.path)
-        print(route.query)
-        match route.path:
-            case "/":
-                self.send_html("index.html")
-            case "/blog":
-                self.send_html("blog.html")
-            case "/log_in":
-                self.send_html("log_in.html")
-            case _:
-                file =  BASE_DIR.joinpath(route.path[1:])
-                if file.exists():
-                    self.send_static(file)
-                else:
-                    self.send_html("error.html", 404)
-                    
-    def do_POST(self):
-        size = self.headers.get('Content-Length')
-        data = self.rfile.read(int(size))
-        print(data)
-        self.send_response(302)
-        self.send_header('Location', '/log_in')
-        self.end_headers()
-
-    def send_html(self, filename, status_code=200):
-        self.send_response(status_code)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-        with open(filename, 'rb') as file:
-            self.wfile.write(file.read())
-
-
-    def send_static(self, filename, status_code=200):
-        self.send_response(status_code)
-        mime_type, *_ = mimetypes.guess_type(filename)
-        if mime_type:
-            self.send_header("Content-Type", mime_type)
+        pr_url = urllib.parse.urlparse(self.path)
+        if pr_url.path == '/':
+            self.send_html_file('index.html')
+        elif pr_url.path == '/log_in':
+            self.send_html_file('log_in.html')
+        elif pr_url.path == '/blog':
+            self.send_html_file('blog.html')
         else:
-            self.send_header("Content-Type", "text/plain")
+            if pathlib.Path().joinpath(pr_url.path[1:]).exists():
+                self.send_static()
+            else:
+                self.send_html_file('error.html', 404)
+
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        with open(filename, 'rb') as file:
+        with open(filename, 'rb') as fd:
+            self.wfile.write(fd.read())
+            
+    def send_static(self):
+        self.send_response(200)
+        mt = mimetypes.guess_type(self.path)
+        if mt:
+            self.send_header("Content-type", mt[0])
+        else:
+            self.send_header("Content-type", 'text/plain')
+        self.end_headers()
+        with open(f'.{self.path}', 'rb') as file:
             self.wfile.write(file.read())
+            
+    def do_POST(self):
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        
+        
+        run_socket_client(UDP_IP,UDP_PORT, data=data)
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.end_headers()
 
 
-def run_http_server(host, port):
-    address = (host, port)
-    http_server = HTTPServer(address, GoitFramework)
+def run(server_class=HTTPServer, handler_class=HttpHandler):
+    server_address = ('0.0.0.0', HTTP_PORT)
+    http = server_class(server_address, handler_class)
     try:
-        http_server.serve_forever()
+        http.serve_forever()
     except KeyboardInterrupt:
-        pass
-    finally:
-        http_server.server_close()
-
-
+        http.server_close()
+        
+        
 def run_socket_server(ip, port):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
         addr = ip, port
         server.bind(addr)
         try:
             while True:
-                data, address = server.recvfrom(buffer_size)
+                data, address = server.recvfrom(bufferSize)
                 data_parse = urllib.parse.unquote_plus(data.decode())
                 data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
                 result = {str(datetime.now()): data_dict}
@@ -90,7 +82,7 @@ def run_socket_server(ip, port):
                 server.sendto(data, address)
                 print(f'Send data: {data.decode()} to: {address}')
         except KeyboardInterrupt:
-            print('Bye server')
+            print(f'Destroy server')
             
             
 def run_socket_client(ip: str, port: int, data=None):
@@ -98,14 +90,16 @@ def run_socket_client(ip: str, port: int, data=None):
         server = ip, port
         client.sendto(data, server)
         print(f'Send data: {data.decode()} to server: {server}')
-        response, address = client.recvfrom(buffer_size)
+        response, address = client.recvfrom(bufferSize)
         print(f'Response data: {response.decode()} from address: {address}')
 
 
-main_server = threading.Thread(target=run_http_server, args=(HTTP_HOST, HTTP_PORT))
-socket_server = threading.Thread(target=run_socket_server, args=(UDP_IP, UDP_PORT))
-    
-if __name__ == "__main__":
+main_server = Thread(target=run)
+socket_server = Thread(target=run_socket_server, args=(UDP_IP, UDP_PORT))
+
+
+if __name__ == '__main__':
     main_server.start()
     socket_server.start()
-
+    main_server.join()
+    socket_server.join()
